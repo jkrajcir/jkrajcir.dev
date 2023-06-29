05817b174ec891b42ede98066bffa597f3133a5d
Joe Krajcir <jkrajcir@yahoo.com>
2023-06-28 20:12:31 -0500

    remove eleventy navigation and add prod scripts

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
-
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(eleventyNavigationPlugin)
-
     eleventyConfig.addPassthroughCopy('styles/fonts')
     eleventyConfig.addPassthroughCopy('scripts')
     eleventyConfig.addPassthroughCopy('favicon')
